d9dab474f977de3e47783ee88a87428012b3cc5e
Ravi Ranjan 💻 <ravi.ranjan@oodles.io>
2022-04-28 02:01:30 +0530

    Core data

--- MyCoreData/ContentView.swift
+++ MyCoreData/ContentView.swift
@@ -9,9 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     let coreDataManager: CoreDataManager
+    @State  private var movieName: String = ""
+    @State private var movies:[Movie]  = [Movie]()
+
+    private func populateMovies() {
+        movies = coreDataManager.getAllMovies()
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("Enter movie Name", text: $movieName)
+                .textFieldStyle(.roundedBorder)
+            Button("Save") {
+                coreDataManager.saveMovie(title: movieName)
+                populateMovies()
+            }
+            
+            
+            List {
+                ForEach (movies, id: \.self) {
+                    movie in
+                    Text(movie.title ?? "")
+                }.onDelete { indexSet in
+                    indexSet.forEach { index in
+                        let movie = movies[index]
+                        coreDataManager.deleteMoview(movie: movie)
+                        populateMovies()
+                    }
+                    
+                }
+            }
+            Spacer()
+        }.padding()
+            .onAppear {
+                populateMovies()
+            }
     }
 }
 
--- MyCoreData/Managers/CoreDataManager.swift
+++ MyCoreData/Managers/CoreDataManager.swift
@@ -10,12 +10,53 @@ import CoreData
 class CoreDataManager {
     
     let persistanceContainer : NSPersistentContainer
+    
+    // Created Initilizer
     init(){
         persistanceContainer = NSPersistentContainer(name: "Model")
         persistanceContainer.loadPersistentStores { (description, error) in
             if let error = error {
                 fatalError("Core data Store is Failed to initiate, \(error.localizedDescription)")
             }
+        }
+    }
+    
+    func saveMovie(title: String) {
+        
+        // Need to created object for Movie Entity
+        
+        let movie = Movie(context: persistanceContainer.viewContext)
+        movie.title = title
+        do {
+            try persistanceContainer.viewContext.save()
+        } catch (let err) {
+            print(err.localizedDescription)
+        }
+    }
+    
+    func getAllMovies() -> [Movie] {
+    
+        //get all moview
+        // Creata afetch request
+        let fetchRequest : NSFetchRequest <Movie> = Movie.fetchRequest()
+        
+        do {
+           return try persistanceContainer.viewContext.fetch(fetchRequest)
+        }
+        catch {
+            return[]
+        }
+    }
+    
+    func deleteMoview(movie : Movie) {
+        // Delete single movie from moview
+        // don't confuse here Movie is a model fo single data of moview which is saved
+        persistanceContainer.viewContext.delete(movie)
+        do {
+            try persistanceContainer.viewContext.save()
+        } catch {
+            persistanceContainer.viewContext.rollback()
+            print("failed to save context, error is  :  \(error.localizedDescription)")
         }
     }
 }
